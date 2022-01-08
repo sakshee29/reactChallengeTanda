097349fa747f2554bbdde6bd01f88278d67607ca
Sakshee Tosniwal <toshniwalsakshee2002@gmail.com>
2022-01-08 12:01:23 +1000

    Login Page formed

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -1,10 +1,27 @@
 
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <h1>Adnat</h1>
+        <a href="#"><h1>Adnat</h1></a>
+        <h2>Login</h2>
+        <form>
+          <label>
+            <br></br> Email:<br></br>
+            <input type="text" name="email" />
+          </label>
+
+          <label>
+            <br></br>Password:<br></br>
+            <input type="text" name="email" />
+          </label>
+          <br></br>
+          <input type="submit" value="Login" />
+        </form>
+
+        <a href="#">Signup</a><br></br>
+        <a href="#">Forgot your Password?</a>
+
     </div>
   );
 }
