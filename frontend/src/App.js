b68a8f1d1a56c55c7bc4d12bdb766129652760a3
Sakshee Tosniwal <toshniwalsakshee2002@gmail.com>
2022-01-17 17:31:35 +1000

    BrowserRouter added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import Login from './components/login';
 import Signup from './components/signup';
 import './style.css';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+
 /* NOTE: Style doesn't necessarily need to be imported in the login.jsx file*/
 
 function App() {
   return (
     <div className="App">
-        <Login />
+        <BrowserRouter>
+          
+          <Routes>
+            <Route exact path ="/" element={<Signup />} />
+            <Route path ="/login" element={<Login />}/>
+          </Routes>
+
+        </BrowserRouter>
     </div>
   );
 }
