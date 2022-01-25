6887470138ea7ef976b95a8f2f199001f637be5a
Sakshee Tosniwal <toshniwalsakshee2002@gmail.com>
2022-01-25 10:01:35 +1000

    isLoggedIn prop set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,28 @@ import Header from './components/Header';
 import Dashboard from './pages/dashboard';
 import './style.css';
 import {BrowserRouter,Routes,Route} from "react-router-dom";
+import React from 'react';
+import { useState } from "react";
 
 
 /* NOTE: Style doesn't necessarily need to be imported in the login.jsx file*/
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div className="App">
         <BrowserRouter>
           <Header/>
           <Routes>
             <Route path ="/signup" element={<Signup />} />
-            <Route path ="/login" element={<Login />}/>
-            <Route path ="/dashboard" element={<Dashboard />}/>
+            {!isLoggedIn ? (
+              <Route exact path ="/" element={<Login />}/>
+            ): (
+              <Route exact path ="/" element={<Dashboard />}/>
+            )}
+            
           </Routes>
 
         </BrowserRouter>
