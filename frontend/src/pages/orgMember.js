1dc93080e304c7e1582a11460e817768441652ef
Sakshee Tosniwal <toshniwalsakshee2002@gmail.com>
2022-01-26 16:25:53 +1000

    "Leave Organisation" working!

--- frontend/src/pages/orgMember.js
+++ frontend/src/pages/orgMember.js
@@ -1,13 +1,33 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
+const URL = "http://127.0.0.1:3001";
+
 function OrgMember(props){
+
+    function leaveOrg(){
+
+        const url = `${URL}/organisations/leave`;
+        const sessionId = localStorage.getItem("sessionId");
+
+        return(
+            fetch(url, {
+                method: "POST",
+                headers: {'Authorization': `${sessionId}`, "Content-Type":"application/json"}
+            })
+            .then((res)=> res.json())
+            .then((res)=> {
+                console.log(res);
+            })
+        )
+    }
+
     return(
         <div>
             <h2>{props.orgName}</h2>
             <Link className="memberLinks" to="/">View Shifts</Link>
             <Link className="memberLinks" to="/">Edit</Link>
-            <Link className="memberLinks" to="/">Leave</Link>
+            <button className="memberLinks" onClick={leaveOrg} >Leave</button>
             
         </div>
     )
